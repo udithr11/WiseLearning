5a90bdbff43dbc48a502fce1b0482d23d7ef7c72
udithr11 <udithr11@gmail.com>
2024-03-08 15:21:40 +0530

    contact_started

--- index.js
+++ index.js
@@ -15,3 +15,31 @@ window.addEventListener('scroll', function() {
     }
   });
   
+  window.addEventListener('resize', function() {
+    var container1 = document.querySelector('.responsive_change1');
+    var container2 = document.querySelector('.responsive_change2');
+    var container3 = document.querySelector('.responsive_change3');
+    var container4 = document.querySelector('.responsive_change4');
+    if (window.innerWidth <= 1400) {
+      container1.classList.remove('col-sm-10');
+      container1.classList.add('col-sm-12');
+      container2.classList.remove('col-sm-10');
+      container2.classList.add('col-sm-12');
+      container3.classList.remove('col-sm-10');
+      container3.classList.add('col-sm-12');
+      container4.classList.remove('col-sm-10');
+      container4.classList.add('col-sm-12');
+    } else {
+      container1.classList.remove('col-sm-12');
+      container1.classList.add('col-sm-10');
+      container2.classList.remove('col-sm-12');
+      container2.classList.add('col-sm-10');
+      container3.classList.remove('col-sm-12');
+      container3.classList.add('col-sm-10');
+      container4.classList.remove('col-sm-12');
+      container4.classList.add('col-sm-10');
+    }
+  });
+
+  // Call the event listener once to set the initial state
+  window.dispatchEvent(new Event('resize'));
